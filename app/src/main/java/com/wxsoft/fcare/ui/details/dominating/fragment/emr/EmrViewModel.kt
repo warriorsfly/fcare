f6ee7b54953d27b15e52a8663db29ef66d91a8f6
allen <warriorsfly@gmail.com>
2019-01-02 19:07:36 +0800

    病人信息编辑 xml

--- app/src/main/java/com/wxsoft/fcare/ui/details/dominating/fragment/emr/EmrViewModel.kt
+++ app/src/main/java/com/wxsoft/fcare/ui/details/dominating/fragment/emr/EmrViewModel.kt
@@ -3,5 +3,5 @@ package com.wxsoft.fcare.ui.details.dominating.fragment.emr
 import com.wxsoft.fcare.ui.BaseViewModel
 
 class EmrViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
+
 }
