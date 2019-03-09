e6403ab99cc69c029f121e8a67784cbc566877ea
allen <warriorsfly@gmail.com>
2019-03-09 14:13:57 +0800

    评分详情

---
+++ app/src/main/java/com/wxsoft/fcare/ui/rating/OptionAdapter.kt
@@ -0,0 +1,62 @@
+package com.wxsoft.fcare.ui.rating
+
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.wxsoft.fcare.R
+import com.wxsoft.fcare.core.data.entity.rating.Option
+import com.wxsoft.fcare.core.data.entity.rating.Rating
+import com.wxsoft.fcare.core.data.entity.rating.Subject
+import com.wxsoft.fcare.databinding.ItemRatingSubjectBinding
+import com.wxsoft.fcare.databinding.ItemRatingSubjectItemBinding
+
+
+class OptionAdapter constructor(private val owner: LifecycleOwner,
+                                private val rating:Rating?,
+                                private val subject: Subject):
+    ListAdapter<Option,OptionAdapter.ItemViewHolder>(DiffCallback) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return  ItemViewHolder(
+            ItemRatingSubjectItemBinding.inflate(inflater, parent, false).apply {
+                lifecycleOwner = owner
+            }
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            holder.binding.apply {
+                rat=rating
+                item=getItem(position)
+                index=(position+1).toString()
+                subject=this@OptionAdapter.subject
+                executePendingBindings()
+            }
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<Option>() {
+        override fun areItemsTheSame(oldItem: Option, newItem: Option): Boolean {
+            return  oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Option, newItem: Option): Boolean {
+            return oldItem.checked == newItem.checked
+        }
+    }
+
+    class ItemViewHolder(val binding: ItemRatingSubjectItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
+
+
+
+
