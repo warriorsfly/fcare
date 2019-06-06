c89a00146bd0077b9f80df0ea5af14646d92bb28
allen <warriorsfly@gmail.com>
2019-06-06 11:58:24 +0800

    修改room版本

--- core/src/main/java/com/wxsoft/fcare/core/data/local/AppDatabase.kt
+++ core/src/main/java/com/wxsoft/fcare/core/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import com.wxsoft.fcare.core.data.entity.Patient
 import com.wxsoft.fcare.core.data.local.dao.PatientDao
 
 
-@Database(entities = [Patient::class], version = 1,exportSchema = true)
+@Database(entities = [Patient::class], version = 2,exportSchema = true)
 abstract class AppDatabase:RoomDatabase() {
     abstract fun patientDao(): PatientDao
 }
